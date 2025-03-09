a4595c713d492f389782d136a2e593bfefbf6e13
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2025-03-09 18:21:24 +0530

    update structure

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var loader= document.getElementById("preloader");
-window.addEventListener("load",function(){
-    loader.style.display="none";
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
 });
 
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', () => {
         const navbarCollapse = document.getElementById('navbarNav');
         const navbarToggler = document.querySelector('.navbar-toggler');
-        
+
         // Close the navbar
         if (navbarCollapse.classList.contains('show')) {
             navbarToggler.click();
