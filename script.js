ada8fa42b149837074bb545eccf0a981c2954daf
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2025-03-13 00:43:36 +0530

    ubdate basic website

--- script.js
+++ script.js
@@ -3,40 +3,19 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
 });
 
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-        const navbarCollapse = document.getElementById('navbarNav');
-        const navbarToggler = document.querySelector('.navbar-toggler');
-
-        // Close the navbar
-        if (navbarCollapse.classList.contains('show')) {
-            navbarToggler.click();
-        }
-    });
-});
-
 
 
 
 /***************************************************************************************** */
 
-var typed = new Typed(".text", {
-    strings: ["Front-End Developer", "Fullstack Developer", "Flutter UI Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1200,
-    loop: true
-});
 
-
-
-
-
-
-
-
-
-
+    new Typed(".text", {
+        strings: ["Front-End Developer", "Fullstack Developer", "Flutter UI Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1200,
+        loop: true
+    });
 
 
 
